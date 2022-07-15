abcd022629d3c1d14ca82956a0c4bd80d2eef88f
c0wcarWat3r <cwchin13@gmail.com>
2022-07-15 11:05:49 +0800

    dafd

--- api.py
+++ api.py
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 print('chunwai')
-=======
 import requests 
 
 response = requests.get("http://data.gov.sg")
 print(response.headers.get("content-type"))
->>>>>>> 58c12efd9d0e0e61dc5e82caf5b4e1a4abcc5c21
