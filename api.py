94424b4fcf5bdac130fef5ed745d199f737a2910
c0wcarWat3r <cwchin13@gmail.com>
2022-07-23 10:37:20 +0800

    created main.py, created api_function

--- api.py
+++ api.py
@@ -1,11 +1,13 @@
-import requests 
-## Alpha vantage API Key 
-api_key = "1H49MTNIA7TNIJLO"
+def api_function(): 
+    import requests 
+    ## Alpha vantage API Key 
+    api_key = "1H49MTNIA7TNIJLO"
 
-## Calling API from python 
-url = f"https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey={api_key}"
+    ## Calling API from python 
+    url = f"https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey={api_key}"
 
-response = requests.get(url)
-print(response)
-data = response.json()
-print(data['Realtime Currency Exchange Rate']['5. Exchange Rate'])
+    response = requests.get(url)
+    print(response)
+    data = response.json()
+    print(data['Realtime Currency Exchange Rate']['5. Exchange Rate'])
+api_function()
---
+++ main.py
@@ -0,0 +1,8 @@
+import api, cash_on_hand,overheads,profit_loss
+
+def main(): 
+    forex = api.api_function()
+    overheads.overhead_function(forex)
+    cash_on_hand.coh_function(forex)
+    profit_loss.profitloss_function(forex)
+main()
