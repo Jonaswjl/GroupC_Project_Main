dd262918800f2bcfce197678db16e0b2ff862630
c0wcarWat3r <cwchin13@gmail.com>
2022-07-24 09:51:15 +0800

    a

--- overheads.py
+++ overheads.py
@@ -2,7 +2,8 @@ from pathlib import Path
 import csv 
 
 # instantiate a file path to overheads csv file in current working directory 
-file_path = Path.cwd()/"csv_reports"/"overheads-day-45.csv"
+file_path = Path.cwd()/"csv_reports2"/"overheads.csv"
+
 # open the csv file in read mode 
 with file_path.open(mode="r",encoding="UTF-8",newline="") as file: 
     # create a reader object 
@@ -10,5 +11,4 @@ with file_path.open(mode="r",encoding="UTF-8",newline="") as file:
     # print the contents of the csv file 
     for line in reader: 
         print(line)
-        # print the expenses
         print(line[1])
--- profit_loss.py
+++ profit_loss.py
@@ -1,1 +1,9 @@
-print("yadayada~~")
+from pathlib import Path 
+import csv
+
+file_path = Path.cwd()/"csv_reports2"/"profit and loss.csv"
+
+with file_path.open(mode="r",encoding="UTF-8",newline="") as file: 
+    reader = csv.reader(file)
+    for line in reader: 
+        print(line)
