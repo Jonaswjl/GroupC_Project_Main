31fcb25ee3385c1c79db4343fa32496095acade7
c0wcarWat3r <cwchin13@gmail.com>
2022-07-25 09:02:36 +0800

    added comments to overheads

--- overheads.py
+++ overheads.py
@@ -1,36 +1,40 @@
 from pathlib import Path 
 import csv 
 
-# def overhead_function(): 
+# create an empty list to store the category and value of overheads
 list = []
-
-category = []
+# create an empty list to store the value of overheads
 overheads = []
 
 # instantiate a file path to overheads csv file in current working directory 
-file_path = Path.cwd()/"csv_reports"/"overheads-day-45.csv"
+file_path = Path.cwd()/"csv_reports2"/"overheads.csv"
 
 # open the csv file in read mode 
 with file_path.open(mode="r",encoding="UTF-8-sig",newline="") as file: 
     # create a reader object 
     reader = csv.reader(file)
+    # skip the headers
     next(reader)
+    
     for line in reader:
-        category.append(line[0])
+        # append the value of overheads to the empty list
         overheads.append(float(line[1]))
+        # shift the values in front of category
         line.sort()
-        # print(line)
+        # append the sorted lists to an empty list
         list.append(line)
-# print(list)
-dict = dict(list)
-        
-# print(category)
-max = max(overheads)
-key = str(max)
-# print(type(key))
-# print(dict[key])
 
-print(f"[Highest Overheads] {dict[key]}: SGD{key}")
+# convert the list of sublists into a dictionary 
+dict = dict(list)
+
+# find the max value in the overheads list to see what is the value of the highest overhead 
+max = max(overheads)
+# convert the highest overhead value to a string 
+key = str(max)
+# use the key to find the value which the category of the highest overhead
+value = dict[key]
+
+print(f"[Highest Overheads] {value}: SGD{key}")
 
 
 #         # convert the numbers from string to float 
