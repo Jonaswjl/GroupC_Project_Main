15ee845b860214d57ab442badfed64cf2c780893
c0wcarWat3r <cwchin13@gmail.com>
2022-07-26 20:39:02 +0800

    chun wai

--- test.py
+++ test.py
@@ -1,20 +1,25 @@
+## cash on hand 
 from pathlib import Path 
-import csv
+import csv 
 
-file_path = Path.cwd()/"csv_reports2"/"profit and loss.csv"
+# create an empty list to store cash on hand values 
+coh = []
 
-net_profit=[]
-dailydiff=[]
-
-with file_path.open(mode="r",encoding="UTF-8-sig", newline="") as file: 
+# instantiate a file path to the cash on hand csv file 
+file_path = Path.cwd()/"csv_reports"/"cash-on-hand-thb.csv"
+# open file in read mode
+with file_path.open(mode="r",encoding="UTF-8-sig",newline="") as file: 
+    # create a reader object
     reader = csv.reader(file)
+    # skip headers 
     next(reader)
+    # append cash on hand to list 
     for line in reader:
-        net_profit.append(int(line[4]))
-    #for day 6 i cannot add 1 to create a day 7 so must -1
-    for i in range(len(net_profit)-1):
-        diff=(net_profit[i+1]-net_profit[i])
+        coh.append(int(line[1]))
+    for i in range(len(coh)-1):
+        diff=(coh[i+1]-coh[i])
+        # checks whether difference between days is a negative number, return absolute value of the difference so its not a negative number
         if diff < 0: 
-            print(f"[PROFIT DEFICIT] AMOUNT: {abs(diff)}")
+            print(f"[CASH DEFICIT] AMOUNT: {abs(diff)}")
         else: 
-            print(f"[NET PROFIT SURPLUS] NET PROFIT ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY")
+            print(f"[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY")
