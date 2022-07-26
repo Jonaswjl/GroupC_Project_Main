d4b6c7a2a9944564786001817ac0d662cd59f417
Meganyeye <s10243697@connect.np.edu.sg>
2022-07-26 11:15:01 +0800

    megan

--- profit_loss.py
+++ profit_loss.py
@@ -3,7 +3,16 @@ import csv
 
 file_path = Path.cwd()/"csv_reports2"/"profit and loss.csv"
 
-with file_path.open(mode="r",encoding="UTF-8-sig",newline="") as file: 
+net_profit=[]
+dailydiff=[]
+with file_path.open(mode="r",encoding="UTF-8-sig", newline="") as file: 
     reader = csv.reader(file)
-    for line in reader: 
-        print(line)
+    next(reader)
+    for line in reader:
+        net_profit.append(int(line[4]))
+        
+
+
+
+
+
