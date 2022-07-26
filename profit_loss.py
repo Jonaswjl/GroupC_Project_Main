70becfbadd546c64f70e18d7a253a38221cdd58b
Meganyeye <s10243697@connect.np.edu.sg>
2022-07-27 00:22:49 +0800

    megan

--- profit_loss.py
+++ profit_loss.py
@@ -9,19 +9,21 @@ with file_path.open(mode="r",encoding="UTF-8-sig", newline="") as file:
     reader = csv.reader(file)
     next(reader)
     for line in reader:
-        net_profit.append(int(line[4]))
-
+        net_profit.append([float(line[0])]+[float(line[4])])
+print(net_profit)
 def profit():
-    i=0
-    while i < (len(net_profit)-1):
-        if net_profit[i] < net_profit[i+1]:
-            message="Net profit of each day is higher than the previous day"
-            return message 
-        else:
-            diff= net_profit[i]-net_profit[i+1]
-            print(diff) 
+    for values in net_profit:
+        i=0
+        while i < (len(net_profit)-1):
+            if values[i] < values[i+1]:
+                diff= values[i+1]-values[i]
+                message= f"DAY: {values[i]}, AMOUNT: SGD{diff}"
+                return message
+            else:
+                message="Net profit of each day is higher than the previous day"
+                return message 
 
-
+print(profit())
             
 
 
