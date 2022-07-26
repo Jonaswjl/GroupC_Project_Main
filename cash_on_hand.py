be44d9ec5726482c597ca8c5dfc20d5878529cdd
c0wcarWat3r <cwchin13@gmail.com>
2022-07-26 20:39:45 +0800

    chun wai

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -1,10 +1,26 @@
+## cash on hand 
 from pathlib import Path 
 import csv 
 
+# create an empty list to store cash on hand values 
+coh = []
+
+# instantiate a file path to the cash on hand csv file 
 file_path = Path.cwd()/"csv_reports"/"cash-on-hand-thb.csv"
-print(file_path.exists())
+# open file in read mode
 with file_path.open(mode="r",encoding="UTF-8-sig",newline="") as file: 
+    # create a reader object
     reader = csv.reader(file)
-    for line in reader: 
-        print(line)
+    # skip headers 
+    next(reader)
+    # append cash on hand to list 
+    for line in reader:
+        coh.append(int(line[1]))
+    for i in range(len(coh)-1):
+        diff=(coh[i+1]-coh[i])
+        # checks whether difference between days is a negative number, return absolute value of the difference so its not a negative number
+        if diff < 0: 
+            print(f"[CASH DEFICIT] AMOUNT: {abs(diff)}")
+        else: 
+            print(f"[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY")
 
