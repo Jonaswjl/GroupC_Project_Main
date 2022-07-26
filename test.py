405305bb6e1325626d3930e9fa573c634bb7a7e7
c0wcarWat3r <cwchin13@gmail.com>
2022-07-26 20:26:28 +0800

    chun wai

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ file_path = Path.cwd()/"csv_reports2"/"profit and loss.csv"
 
 net_profit=[]
 dailydiff=[]
+
 with file_path.open(mode="r",encoding="UTF-8-sig", newline="") as file: 
     reader = csv.reader(file)
     next(reader)
@@ -14,6 +15,6 @@ with file_path.open(mode="r",encoding="UTF-8-sig", newline="") as file:
     for i in range(len(net_profit)-1):
         diff=(net_profit[i+1]-net_profit[i])
         if diff < 0: 
-            print(f"[PROFIT DEFICIT] AMOUNT: {diff}")
+            print(f"[PROFIT DEFICIT] AMOUNT: {abs(diff)}")
         else: 
             print(f"[NET PROFIT SURPLUS] NET PROFIT ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY")
