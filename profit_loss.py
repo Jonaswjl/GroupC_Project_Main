0554781428190a6d845b00387bd99fbdc8953607
Meganyeye <s10243697@connect.np.edu.sg>
2022-07-27 14:32:58 +0800

    m

--- profit_loss.py
+++ profit_loss.py
@@ -15,12 +15,13 @@ with file_path.open(mode="r",encoding="UTF-8", newline="") as file:
     next(reader)
     # append values of net profit to empty list
     for line in reader:
-        net_profit.append([float(line[0])]+[float(line[4])])
-print(net_profit)
-def profit():
-    for values in net_profit:
+        net_profit.append([line[0]]+[float(line[4])])
+np= dict(net_profit)
+print(np)
+def profit():   
+    for values in np:
         i=0
-        while i < (len(net_profit)-1):
+        while i < (len(np)-1):
             if values[i] < values[i+1]:
                 diff= values[i+1]-values[i]
                 message= f"DAY: {values[i]}, AMOUNT: SGD{diff}"
@@ -31,15 +32,15 @@ def profit():
 
 print(profit())
             
-net_profit.append(int(line[4]))
-    #for day 6 i cannot add 1 to create a day 7 so must -1
-for i in range(len(net_profit)-1):
-        diff=(net_profit[i+1]-net_profit[i])
-        # checks whether difference between days is a negative number, return absolute value of the difference so its not a negative number
-        if diff < 0: 
-            print(f"[PROFIT DEFICIT] AMOUNT: SGD{abs(diff)}")
-        else: 
-            print(f"[NET PROFIT SURPLUS] NET PROFIT ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY")
+# net_profit.append(int(line[4]))
+#     #for day 6 i cannot add 1 to create a day 7 so must -1
+# for i in range(len(net_profit)-1):
+#         diff=(net_profit[i+1]-net_profit[i])
+#         # checks whether difference between days is a negative number, return absolute value of the difference so its not a negative number
+#         if diff < 0: 
+#             print(f"[PROFIT DEFICIT] AMOUNT: SGD{abs(diff)}")
+#         else: 
+#             print(f"[NET PROFIT SURPLUS] NET PROFIT ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY")
 
 
 
