f27e34c3da318110afae89d4f063ffb1c9963c31
c0wcarWat3r <cwchin13@gmail.com>
2022-07-28 21:39:19 +0800

    c

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import api, cash_on_hand,overheads,profit_loss
 
 def main(): 
-    api.api_function()
-    overheads.overhead_function()
-    cash_on_hand.coh_function()
-    profit_loss.profitloss_function()
+    try: 
+        api.api_function()
+        overheads.overhead_function()
+        cash_on_hand.coh_function()
+        profit_loss.profitloss_function()
+    except KeyError: 
+        pass
 main()
