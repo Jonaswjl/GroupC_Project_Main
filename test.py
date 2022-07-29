f26656ed4936b2eaaf927ddc8678c9c4ea9c0b06
c0wcarWat3r <cwchin13@gmail.com>
2022-07-29 08:49:39 +0800

    c

--- test.py
+++ test.py
@@ -7,11 +7,6 @@ url = f"https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_c
 
 response = requests.get(url)
 data = response.json()
-# rate = data['Realtime Currency Exchange Rate']['5. Exchange Rate']
-rate = data.get('Realtime Currency Exchange Rate').get('5. Exchange Rate')
+rate = data['Realtime Currency Exchange Rate']['5. Exchange Rate']
 rate = float(rate)
 print(f"[REAL TIME CURRENCY CONVERSION RATE] USD1 = SGD{rate}")
-    
-    
-
-
