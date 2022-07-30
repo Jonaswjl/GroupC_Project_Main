219f7e54b8737d879fbee8c04e39a6faaccb98a2
Meganyeye <s10243697@connect.np.edu.sg>
2022-07-30 08:45:38 +0800

    m

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import api, cash_on_hand,overheads,profit_loss
-
+from pathlib import Path
 def main(): 
     api.api_function()
     overheads.overhead_function()
     cash_on_hand.coh_function()
     profit_loss.profitloss_function()
-main()
+
+file_path=  Path.cwd()/"Summary_report.txt"
+file_path.touch()
+
