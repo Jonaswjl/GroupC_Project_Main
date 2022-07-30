07f1cace338f99c45eb0824b351c2fa2723acfa1
c0wcarWat3r <cwchin13@gmail.com>
2022-07-30 21:56:11 +0800

    c

--- test.py
+++ test.py
@@ -1,19 +1,56 @@
-import api, cash_on_hand,overheads,profit_loss
-from pathlib import Path
-# def main(): 
-#     api.api_function()
-#     overheads.overhead_function()
-#     cash_on_hand.coh_function()
-#     profit_loss.profitloss_function()
+from pathlib import Path 
+import csv
+import api 
 
-file_path=  Path.cwd()/"Summary_report.txt"
+forex = api.rate_function()
+
+# instantiate a file path to the profit and loss csv file 
+# file_path = Path.cwd()/"csv_reports_game"/"profit-and-loss-usd-42.csv"
+file_path = Path.cwd()/"csv_reports2"/"profit and loss.csv"
+
+# create an empty list to append net profit 
+net_profit=[]
+dd=[]
+d=[]
+result = []
+# open file in read mode 
+with file_path.open(mode="r",encoding="UTF-8", newline="") as file: 
+    # create a reader object
+    reader = csv.reader(file)
+    # skip the headers
+    next(reader)
+    for line in reader:
+        # append only the net profits for each day to the empty list and convert them to floats (so diff can be calculated)
+        net_profit.append(float(line[4]))
+        d.append(line[0])
+day=(len(net_profit)-1)  
+# i is an index (eg 0-5) range gives 0 to the highest index (in this case 6 days will have indices 0-5 )
+for i in range(day):
+    diff=(net_profit[i+1]-net_profit[i])
+    # append the list of days and list of differences
+    # combine the day no. when there is a profit deficit with the diff in a nested list
+    dd.append([d[i+1]]+[diff])
+
+#convert nested list into a dictionary where day number is the key and profit difference is the value 
+dictionary=dict(dd)
+is_positive= True
+for pd in dictionary:
+    #dict[pd] accesses the values (profit diff) and pd is the key 
+    if dictionary[pd] < 0 : 
+        # is_positive= False
+        # return f"[PROFIT DEFICIT] DAY: {pd} AMOUNT: SGD{round(abs(dictionary[pd]*forex),1)}"
+        result.append(f"[PROFIT DEFICIT] DAY: {pd} AMOUNT: SGD{round(abs(dictionary[pd]*forex),1)}\n")
+        is_positive= False
+if is_positive==True:
+        # return f"[NET PROFIT SURPLUS] NET PROFIT ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY"
+        result.append(f"[NET PROFIT SURPLUS] NET PROFIT ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY")
+    
+file_path =  Path.cwd()/"test.txt"
 file_path.touch()
-# main()
-i = api.api_function()
-print(i)
-with file_path.open(mode="w", encoding = "UTF-8") as file: 
-    file.writelines(i)
-    
-    
+if file_path.exists(): 
+    with file_path.open(mode="w", encoding = "UTF-8") as file: 
+        file.writelines(result)
+else: 
+    print("file doesn't exist")
 
 
