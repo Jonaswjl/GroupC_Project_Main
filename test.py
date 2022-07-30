c793f7c3ef9ddd1eb05ff7120a070dd01a7ead80
c0wcarWat3r <cwchin13@gmail.com>
2022-07-30 08:40:54 +0800

    c

--- test.py
+++ test.py
@@ -1,15 +1,35 @@
-import requests 
-## Alpha vantage API Key 
-api_key = "1H49MTNIA7TNIJLO"
+# create a function to find the exchange rate between SGD and USD
+def api_function(): 
+    import requests 
+    ## Alpha vantage API Key 
+    api_key = "1H49MTNIA7TNIJLO"
 
-## Calling API from python 
-url = f"https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey={api_key}"
+    ## Calling API from python 
+    url = f"https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey={api_key}"
 
-response = requests.get(url)
-data = response.json()
-rate = data['Realtime Currency Exchange Rate']['5. Exchange Rate']
-rate = float(rate)
-print(f"[REAL TIME CURRENCY CONVERSION RATE] USD1 = SGD{rate}")
+    response = requests.get(url)
+    data = response.json()
+    # rate = data['Realtime Currency Exchange Rate']['5. Exchange Rate']
+    for info in data:
+        rate=data[info]['5. Exchange Rate']
+    rate = float(rate)
+    print(f"[REAL TIME CURRENCY CONVERSION RATE] USD1 = SGD{rate}")
+
+def rate_function():
+    import requests 
+    ## Alpha vantage API Key 
+    api_key = "1H49MTNIA7TNIJLO"
+
+    ## Calling API from python 
+    url = f"https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey={api_key}"
+
+    response = requests.get(url)
+    data = response.json()
+    for info in data:
+        rate=data[info]['5. Exchange Rate']
+    # rate = data['Realtime Currency Exchange Rate']['5. Exchange Rate']
+    rate = float(rate)
+    return rate
     
     
 
