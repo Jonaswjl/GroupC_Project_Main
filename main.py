2f6d1dda6bba491caeb819e06b0ac32c6609fb76
Meganyeye <s10243697@connect.np.edu.sg>
2022-08-02 10:18:16 +0800

    m

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 import api, cash_on_hand,overheads,profit_loss
 from pathlib import Path
 
-a = api.api_function()
-b = overheads.overhead_function()
-c = cash_on_hand.coh_function()
-d = profit_loss.profitloss_function()
+forex = api.rate_function
+print(forex)
+# a = api.api_function()
+# b = overheads.overhead_function(forex)
+# c = cash_on_hand.coh_function(forex)
+# d = profit_loss.profitloss_function(forex)
 
-file_path=  Path.cwd()/"Summary_report.txt"
-file_path.touch()
-with file_path.open(mode="w", encoding = "UTF-8") as file: 
-    file.write(a)
-    file.write("\n")
-    file.write(b)
-    file.write("\n")
-    file.writelines(c)
-    file.write("\n")
-    file.writelines(d)
+# file_path=  Path.cwd()/"Summary_report.txt"
+# file_path.touch()
+# with file_path.open(mode="w", encoding = "UTF-8") as file: 
+#     file.write(a)
+#     file.write("\n")
+#     file.write(b)
+#     file.write("\n")
+#     file.writelines(c)
+#     file.write("\n")
+#     file.writelines(d)
 
