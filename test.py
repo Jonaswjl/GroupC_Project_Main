0b0f8f34ec5965c7e8540d68e869dba1ba54f7c8
c0wcarWat3r <cwchin13@gmail.com>
2022-08-02 10:28:26 +0800

    c

---
+++ test.py
@@ -0,0 +1,18 @@
+
+import requests 
+## Alpha vantage API Key 
+api_key = "1H49MTNIA7TNIJLO"
+
+## Calling API from python 
+url = f"https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey={api_key}"
+
+response = requests.get(url)
+data = response.json()
+outer_key = 'Realtime Currency Exchange Rate'
+if outer_key in data:
+    rate= data['Realtime Currency Exchange Rate']['5. Exchange Rate']
+    rate = float(rate)
+    print(rate)
+else: 
+    print(data)
+    print(data["Note"])
